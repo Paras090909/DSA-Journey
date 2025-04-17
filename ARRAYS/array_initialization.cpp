44e6ccd09eb473908c03de0228d1bec1375676e0
Paras Dewangan <dewparas56@gmail.com>
2025-04-17 19:02:21 +0530

    Update array_initialization.cpp

--- ARRAYS/array_initialization.cpp
+++ ARRAYS/array_initialization.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int main(){
     int size;
+    // for size of the array
     cout<<"enter size"<<endl;
     cin>>size;
-    vector <int> arr(size);
-
+    //initializing array with the size
+    int arr[size];
+    cout<<"enter array elements"<<endl;
     for(int i = 0; i< size; i++){
         cin>>arr[i];
     }
-
-    cout<<"array"<<endl;
+        //printing elements of array
+    cout<<"array elements "<<endl;
     for(int i = 0; i< size; i++){
         cout<<arr[i]<<" ";
     }
