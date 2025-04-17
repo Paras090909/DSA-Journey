d788930c99db1cb7260fdbb95a6968742e1703a0
Paras Dewangan <dewparas56@gmail.com>
2025-04-17 19:24:55 +0530

    Update average_of_array_element.cpp

--- ARRAYS/average_of_array_elements/average_of_array_element.cpp
+++ ARRAYS/average_of_array_elements/average_of_array_element.cpp
@@ -1,22 +1,25 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int main(){
     int size;
     cout<<"enter size"<<endl;
     cin>>size;
-    vector<int> arr(size);
-    cout<<"ente element"<<endl;
+     int arr[size];
+    cout <<"enter  element" << endl;
+
+    
     for(int i = 0; i< size; i++){
         cin>>arr[i];
     }
+    
     int sum = 0;
 
-    cout<<"array"<<endl;
+    
     for(int i = 0; i< size; i++){
         sum += arr[i];
+        
     }
     int avg = sum / size;
-    cout<<"average : "<< avg<<endl;
+    cout<<"average value of array elements: " << avg << endl;
 }
