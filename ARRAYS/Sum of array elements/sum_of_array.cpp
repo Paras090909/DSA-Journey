7b10f8c0e92e36291353177776c374a16be7493b
Paras Dewangan <dewparas56@gmail.com>
2025-04-17 19:14:52 +0530

    Update sum_of_array.cpp

--- ARRAYS/Sum of array elements/sum_of_array.cpp
+++ ARRAYS/Sum of array elements/sum_of_array.cpp
@@ -1,21 +1,22 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int main(){
     int size;
     cout<<"enter size"<<endl;
     cin>>size;
-    vector <int>arr(size);
+    int arr[size];
+    int sum = 0;
+    
     cout<<"enter element"<<endl;
     for(int i = 0; i< size; i++){
         cin>>arr[i];
     }
-    int sum = 0;
+  
 
    
     for(int i = 0; i< size; i++){
         sum += arr[i];
     }
-    cout<<"sum : "<<sum<<endl;
+    cout<<"sum of elements is : " << sum << endl;
 }
