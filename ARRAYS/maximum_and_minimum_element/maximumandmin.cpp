b56bfd72517f0b8cc8704964d9ff7490dbe6a785
Paras Dewangan <dewparas56@gmail.com>
2025-04-19 22:43:20 +0530

    Update maximumandmin.cpp

--- ARRAYS/maximum_and_minimum_element/maximumandmin.cpp
+++ ARRAYS/maximum_and_minimum_element/maximumandmin.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<vector>
+
 using namespace std;
 
 int main(){
@@ -7,26 +7,29 @@ int main(){
     cout<<"enter size"<<endl;
     cin>>size;
     
-    vector <int> arr(size);
-
+    int arr[size];
+    cout<<"enter array elements"<<endl;
     for(int i = 0; i< size; i++){
         cin>>arr[i];
     }
 
-    int maximu = arr[0];
+    int maximum = arr[0];
     int minimum = arr[0];
 
+       // i have used same loop for checking both minimum and maximum element, you can alternatively use two loops also
     for(int j= 0; j< size; j++){
+        // checking for minimum element
        if(arr[j] < minimum){
         minimum = arr[j];
        }
 
-       if(arr[j] > maximu){
-        maximu = arr[j];
+        // checking for maximum element
+       if(arr[j] > maximum){
+        maximum = arr[j];
        }
     }
 
-    cout<<"maximum: "<<maximu<<endl;
+    cout<<"maximum: "<<maximum<<endl;
     cout<<"minimum: "<<minimum<<endl;
 
 }
