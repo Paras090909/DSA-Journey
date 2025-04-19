335e7706ac44694ab8adf9ab8bee51fbcac5f447
Paras Dewangan <dewparas56@gmail.com>
2025-04-19 22:51:26 +0530

    Update search.cpp

--- ARRAYS/linear_search_in_array/search.cpp
+++ ARRAYS/linear_search_in_array/search.cpp
@@ -1,25 +1,28 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int main(){
     int size;
     cout<<"enter size"<<endl;
     cin>>size;
-    vector <int> arr(size);
+    int arr[size];
 
+    cout<<"enter elements of array " <<endl;
     for(int i = 0; i< size; i++){
         cin>>arr[i];
     }
-    int key;
+    
+    int target;
 
     cout<<"enter the element to search"<<endl;
-    cin>>key;
-
+    cin>>target;
     
     for(int i = 0; i< size; i++){
-        if(key == arr[i]){
-            cout<<"array at index "<<i  <<endl;
+        if(target == arr[i]){
+            cout<<"array at index "<< i <<endl;
+        }
+        else{
+           cout<<"target is not in array "<<endl;
         }
     }
 }
